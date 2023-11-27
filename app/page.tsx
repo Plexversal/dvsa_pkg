56c76fc0a799951def97c549f05b6848ffff0554
Plexversal <paddyjo2011@hotmail.co.uk>
2023-11-27 11:52:04 +0000

    v1.0.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 'use client';
 import styles from "../styles/page.module.css"
 import { useState } from 'react'
-import { errors } from '../lib/common_errors'
+import { errors, uncommon_errors, eventsToIgnore } from '../lib/common_errors'
 type LineWithHighlight = {
   highlightedLine: JSX.Element;
   lineNumber: number;
 };
 export default function Home() {
   const [sbFile, setSbFile] = useState<File>()
-  const [sbOutput, setSbOutput] = useState<LineWithHighlight[]>([]);
+  const [testLoadEvents, setTestLoadEvents] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState(false);
+  const [sbAllErrorsOutput, setSbAllErrorsOutput] = useState<LineWithHighlight[]>([]);
+  const [sbUncommonErrors, setSbUncommonErrors] = useState<LineWithHighlight[]>([]);
+  const [networkError, setNetworkError] = useState<number>(0)
+  const [longTime, setLogTime] = useState<number>(0)
+  const [whiteScreens, setWhiteScreens] = useState<number>(0)
+  const [anaylized, setAnaylized] = useState<boolean>(false)
+
+
+  const [sbSlowToLoad, setSbSlowToLoad] = useState<LineWithHighlight[]>([])
 
 
   async function onSubmit (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     if (!sbFile) return alert('Sb Log is required')
-
+    setIsLoading(true)
     const fileReader = new FileReader();
 
 
@@ -22,24 +32,88 @@ export default function Home() {
       const fileContent = event.target?.result;
       if (typeof fileContent === 'string') {
         const lines = fileContent.split(/\r\n|\n/); // split text file based on new lines and form array of strings
-        const searchStrings = errors // predefined errors to search for
+        setTestLoadEvents(lines.filter(e => e.includes("INFO: Application Exit")).length)
     
+        let fluctuationCount = 0;
+        let longTimeEnd = 0;
+        let whiteScreen = 0
+
         const matchedLines = lines.reduce<LineWithHighlight[]>((acc, line, index) => {
-          if (searchStrings.some(str => line.includes(str))) {
-            const highlightedLine = highlightText(line, searchStrings);
+          if (errors.some(str => line.includes(str))) {
+            const highlightedLine = highlightText(line, errors);
             acc.push({ highlightedLine, lineNumber: index + 1 });
+          }
+
+          return acc;
+        }, []);
+        const uncommonErrors = lines.reduce<LineWithHighlight[]>((acc, line, index) => {
+          if (uncommon_errors.some(str => line.includes(str))) {
+            const highlightedLine = highlightText(line, uncommon_errors);
+            acc.push({ highlightedLine, lineNumber: index + 1 });
+          }
+          if (line.includes("network fluctuation")) {
+            fluctuationCount++;
           }
           return acc;
         }, []);
+        setNetworkError(fluctuationCount)
+
+        const threshold = 1000 * 60 * 3; // 2 minutes in milliseconds
+        let previousLine = lines[0];
+        let previousDate:any = parseDate(previousLine);
+        let previousIndex = 0;
     
-        setSbOutput(matchedLines);
+        const matchedSlowed = lines.slice(1).reduce<LineWithHighlight[]>((acc, line, index) => {
+          const currentDate:any = parseDate(line);
+          if (previousDate && currentDate) {
+            const timeDiff = currentDate - previousDate;
+            if (timeDiff > threshold && !eventsToIgnore.some(str => line.includes(str))) {
+              const timeDiffFormatted = formatTimeDiff(timeDiff);
+              whiteScreen++
+              // Get 5 lines before and 5 lines after
+              const start = previousIndex - 2; // 5 lines before the previous line
+              const end = index + 4; // 5 lines after the current line
+      
+              const contextLines = getRangeOfLines(lines, start, end).map((contextLine:any, ctxIndex:any) => {
+                const isHighlighted = ctxIndex === 2 || ctxIndex === 3; // Highlight the 6th and 7th lines (0-based index)
+                const lineNum = start + ctxIndex + 1; // Correct line number
+                if (contextLine.includes('Finish.aspx')) {
+                  longTimeEnd++;
+                }
+                return {
+                  highlightedLine: isHighlighted ? highlightText(`${timeDiffFormatted} ${contextLine}`, [timeDiffFormatted]) : contextLine,
+                  lineNumber: lineNum
+                };
+              });
+      
+              acc.push(...contextLines); // Spread to add all context lines
+            }
+          }
+          previousLine = line;
+          previousDate = currentDate;
+          previousIndex = index + 1;
+          return acc;
+        }, []);
+
+        setLogTime(longTimeEnd)
+        setWhiteScreens(whiteScreen)
+    
+        setSbAllErrorsOutput(matchedLines);
+        setSbUncommonErrors(uncommonErrors)
+        setSbSlowToLoad(matchedSlowed);
+        setIsLoading(false)
+        setAnaylized(true)
       } else {
         alert('Error reading file content');
+        setIsLoading(false)
+
       }
     };
 
     fileReader.onerror = function() {
         alert('Error reading file');
+        setIsLoading(false)
+
     };
 
     fileReader.readAsText(sbFile);
@@ -56,6 +130,25 @@ export default function Home() {
     );
   }
   
+  function parseDate(line: string) {
+    const datePattern = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{4}/;
+    const match = line.match(datePattern) 
+    return match ? new Date(match[0]) : null
+  }
+
+  function formatTimeDiff(milliseconds: number) {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  }
+
+  function getRangeOfLines(lines:any, start:any, end:any) {
+    return lines.slice(Math.max(start, 0), Math.min(end, lines.length));
+  }
+
+  // logs for specific sections only
+
 
   return (
     <main className={styles.main}>
@@ -84,26 +177,121 @@ export default function Home() {
         </form>
         <input form="input-form" type="submit" />
       </div>
-      <h2>Sb Log Analysis</h2>
-      <div className={styles["output-container"]}>
-        <div className={styles["output-content"]}>
-          <div className={styles["line-numbers"]}>
-            {sbOutput.map(({ lineNumber }, i) => (
-              <div key={i} className={styles["line-number"]}>
-                {lineNumber}
-              </div>
-            ))}
-          </div>
-          <div className={styles["code-content"]}>
 
-            {sbOutput.map(({ highlightedLine }, i) => (
-              <code key={i} className={styles["code-text"]}>
-                {highlightedLine}
-              </code>
-            ))}
+      {
+        isLoading ? <div>Loading</div> : <div>
+              {
+                anaylized && <div className={styles['general-info']}>
+                <h2>General Info</h2>
+
+                <table>
+                  <tr>
+                    <th>Exam Load Events</th>
+                    <td>{testLoadEvents}</td>
+
+                  </tr>
+                  <tr>
+                    <th>Network fluctuations found</th>
+                    <td>{networkError > 0 ? networkError : 'None Found'}</td>
+
+                  </tr>
+                  <tr>
+                    <th>White screens</th>
+                    <td>{longTime > 0 ? longTime : 'None Suspected'}</td>
+                  </tr>
+                  <tr>
+                    <th>Long time to clear on test end</th>
+                    <td>{longTime > 0 ? longTime : 'None Suspected'}</td>
+                  </tr>
+                </table>
+              </div>
+              }
+          {
+            sbSlowToLoad.length > 0 && <div>
+
+
+              <h2>Sb Log Suspected slow to load</h2>
+              <p><i>More than 3 mins and excludes time between candidates</i></p>
+            <div className={styles["output-container"]}>
+              <div className={styles["output-content"]}>
+                <div className={styles["line-numbers"]}>
+                  {sbSlowToLoad.map(({ lineNumber }, i) => (
+                    <div key={i} className={styles["line-number"]}>
+                      {lineNumber}
+                    </div>
+                  ))}
+                </div>
+                <div className={styles["code-content"]}>
+  
+                  {sbSlowToLoad.map(({ highlightedLine }, i) => (
+                    <code key={i} className={styles["code-text"]}>
+                      {highlightedLine}
+                    </code>
+                  ))}
+                </div>
+              </div>
+            </div>
+            </div>
+          }
+        {
+          sbUncommonErrors.length > 0 && <div>
+          <h2>Sb uncommon Log Errors Found</h2>
+          <div className={styles["output-container"]}>
+            <div className={styles["output-content"]}>
+              <div className={styles["line-numbers"]}>
+                {sbUncommonErrors.map(({ lineNumber }, i) => (
+                  <div key={i} className={styles["line-number"]}>
+                    {lineNumber}
+                  </div>
+                ))}
+              </div>
+              <div className={styles["code-content"]}>
+
+                {sbUncommonErrors.map(({ highlightedLine }, i) => (
+                  <code key={i} className={styles["code-text"]}>
+                    {highlightedLine}
+                  </code>
+                ))}
+              </div>
+            </div>
           </div>
+          </div>
+          }
+                  
+        {
+          sbAllErrorsOutput.length > 0 && <div>
+          <h2>Sb All Log Errors Found</h2>
+          <p><i>Most errors here are normal and expected</i></p>
+
+          <div className={styles["output-container"]}>
+            <div className={styles["output-content"]}>
+              <div className={styles["line-numbers"]}>
+                {sbAllErrorsOutput.map(({ lineNumber }, i) => (
+                  <div key={i} className={styles["line-number"]}>
+                    {lineNumber}
+                  </div>
+                ))}
+              </div>
+              <div className={styles["code-content"]}>
+
+                {sbAllErrorsOutput.map(({ highlightedLine }, i) => (
+                  <code key={i} className={styles["code-text"]}>
+                    {highlightedLine}
+                  </code>
+                ))}
+              </div>
+            </div>
+          </div>
+          </div>
+          }
+          
+        
+
+
         </div>
-      </div>
+      }
+
+      
     </main>
   );
 }
--- lib/common_errors.tsx
+++ lib/common_errors.tsx
@@ -1,4 +1,15 @@
 export const errors: Array<string> = [
     "Uncaught ReferenceError:",
-    "Uncaught TypeError:"
+    "Uncaught TypeError:",
+
+]
+
+export const uncommon_errors: Array<string> = [
+    "'data_0' is denied.",
+    "network fluctuation"
+]
+
+export const eventsToIgnore: Array<string> = [
+    "INFO: Application Exit",
+    "INFO: Load"
 ]
